72b367f1371c04c356d52582e056b4b511538e8c
machiavellai <oghenevictor54p@gmail.com>
2025-08-02 16:42:21 +0100

    More on primitive values

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,39 @@
+import  java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        //accepting input with Scanner
+        //system.in is to read the input
+        Scanner scanner = new Scanner(System.in);
 
-        int year = 2025 ;
-        int age = 34;
+       //creating a prompt to prompt users to enter their names
+        System.out.print("Enter your Name: ");
+        //declaration and Assignment
+        String name = scanner.nextLine();
 
-        double gap = 22.0;
-        double GPA = 2.9;
-        double price = 11000.99;
+        System.out.println("Hello " + name);
 
-        //when using char, it's a single object like a grade or symbol
-        char grade = 'A';
-        char symbol = '!';
-        char Currency = '$';
+        System.out.print("Enter your Age: ");
+        int age = scanner.nextInt();
 
-        boolean forSale = true;
-        boolean isOnline = true;
-        String name = "victor Oghene";
-        System.out.println(" Hello " + name);
-        String food = "pizza";
-        System.out.println("Your Favourite Food is: " + food);
 
-        String email = "fake123@gmail.comn";
-        String car = "Avalon";
-        String color  = "Orange";
+        System.out.print(" What is your GPA:? ");
+        double Gpa = scanner.nextDouble();
 
-        System.out.println(" your choice of car is a " + " " + color + " " +  year +" " + car);
-        System.out.println("The Price is: "+ price );
+        System.out.println("Are you a Student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean()
+;
+        System.out.println("Hello you are " + name + " And you are " + age + " Years old");
+        System.out.println("Your GPA is : " + Gpa);
+
+        if(isStudent){
+            System.out.println("Student: "+ isStudent);
+        }else{
+            System.out.println(" No Acess! " + "You are not Enrolled");
+        }
+        scanner.close();
+        //it is good to close your scanner at the end of your application
     }
 }
