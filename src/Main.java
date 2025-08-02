d628f3a5b62f0d4e98808a16a3e486beeda5905c
machiavellai <oghenevictor54p@gmail.com>
2025-08-02 15:51:48 +0100

    More on primitive values

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,26 @@ public class Main {
         int age = 34;
 
         double gap = 22.0;
-        double temprature = 222.0;
+        double GPA = 2.9;
+        double price = 11000.99;
 
         //when using char, it's a single object like a grade or symbol
         char grade = 'A';
         char symbol = '!';
+        char Currency = '$';
 
-        boolean isStudent = true;
-        boolean isOnline = false;
-        boolean forSale = false;
+        boolean forSale = true;
+        boolean isOnline = true;
+        String name = "victor Oghene";
+        System.out.println(" Hello " + name);
+        String food = "pizza";
+        System.out.println("Your Favourite Food is: " + food);
 
-        //The boolean condition is similar to JavaScript
-        if(isStudent && forSale){
-            System.out.println("You are a student " + "It is on sale for ss3 students" );
-        }
-        else {
-            System.out.println("Not available for non student");
-        }
-        //
-        System.out.println(" my Grade last year was " + grade + symbol);
-        System.out.println();
-        System.out.println( " the Price is " + temprature + "$ " );
-        System.out.println(" The year is " + year + " " + age);
+        String email = "fake123@gmail.comn";
+        String car = "Avalon";
+        String color  = "Orange";
+
+        System.out.println(" your choice of car is a " + " " + color + " " +  year +" " + car);
+        System.out.println("The Price is: "+ price );
     }
 }
