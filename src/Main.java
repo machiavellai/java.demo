6f5523559c0dc22fe15925c8093430d707f41241
machiavellai <oghenevictor54p@gmail.com>
2025-08-02 17:22:07 +0100

    scanner examples

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,7 @@ import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-     //common issues
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your AGe: ");
-        int age = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Enter your Favorite color ");
-        String color = scanner.nextLine();
-
-        System.out.println("Enter your Favorite car ");
-        String car = scanner.nextLine();
-
-        System.out.println( "you are "+ age + " Years old");
-        System.out.println("you like the color " + color);
-        System.out.println("you like the car " + car);
-
-
-        scanner.close();
-        //it is good to close your scanner at the end of your application
+     //to close your scanner at the end of your application
+        //application of scanner to calaculate area of rectangle
     }
 }
