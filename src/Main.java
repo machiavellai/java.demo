d9488d56976d9714c88566ac0c98f106dbcd1331
machiavellai <oghenevictor54p@gmail.com>
2025-08-03 00:25:28 +0100

    shopping mall app

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,36 @@ import  java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // ORDER OF OPERATIONS [P-E-M-D-A-S]
-//        P - Parenthesis
-//           E - Exponent
-//                M- Multiplication
-//                        D-Division
-//                                A-Addition;
-//                                    S-Substraction;
-        double result = 3 +4 * (7-5) /2.0;
-        System.out.println(result);
-        //in what order is this being solved
-        //from the left to right solve what is in parenthesis
+       //A Shopping Programme
+        //we will use a scanner for input for what users want!
+
+        Scanner scanner = new Scanner((System.in));
+
+        String item ;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total ;
+
+        System.out.print("what Item would you like to buy : ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price of each  : ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many woukd you like? : ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("\nCustomer your total amount for "+ quantity + " "+ item+ " is : " +total);
+        System.out.println( "Your total amount = " + total + " " + currency
+        );
+//        System.out.println(price
+//        );
+
+
+
+        scanner.close();
     }
 }
