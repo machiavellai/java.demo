5cd3940ac07a6e45965f81bec9d7e46da910a197
machiavellai <oghenevictor54p@gmail.com>
2025-08-02 19:48:03 +0100

    Arithmetic Operators

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,14 @@ import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-     //MAD LIBS GAME
-        //scanner will help us accept user inputs
-        Scanner scanner = new Scanner(System.in);
+    //Arithmetic Operators
+        //Augmented Assignment Operators
+        int x = 10;
+        int y = 4;
+        int z;
 
-        String Adjective;
-        String noun1;
-        String Adjective2;
-        String verb;
-        String Adjective3;
+        x %= y;
 
-        System.out.print("Enter An adjective (description):");
-        Adjective = scanner.nextLine();
-        System.out.print("Enter a Noun (animal or person): ");
-        noun1 = scanner.nextLine();
-        System.out.print("Enter An adjective (description): ");
-        Adjective2 = scanner.nextLine();
-        System.out.print("Enter a verb end with -ing(action: )");
-        verb = scanner.nextLine();
-        System.out.print("Enter An adjective (description): ");
-        Adjective3 = scanner.nextLine();
-
-
-        System.out.println("\nToday i went to a "+ Adjective + " zoo" );
-        System.out.println("In an exhibit, i saw a " + noun1 + ".");
-
-        System.out.println(noun1 +"was" + Adjective2 + " and " + verb + "!");
-        System.out.println("I was " + Adjective3 + "!");
-
-
-scanner.close();
+        System.out.println(x);
     }
 }
