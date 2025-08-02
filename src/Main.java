123f74ab700680ea4cb18b31b3168668bfcd74f9
machiavellai <oghenevictor54p@gmail.com>
2025-08-02 17:20:21 +0100

    scanner common issues

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,24 @@ import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //accepting input with Scanner
-        //system.in is to read the input
+     //common issues
         Scanner scanner = new Scanner(System.in);
 
-       //creating a prompt to prompt users to enter their names
-        System.out.print("Enter your Name: ");
-        //declaration and Assignment
-        String name = scanner.nextLine();
-
-        System.out.println("Hello " + name);
-
-        System.out.print("Enter your Age: ");
+        System.out.print("Enter your AGe: ");
         int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter your Favorite color ");
+        String color = scanner.nextLine();
+
+        System.out.println("Enter your Favorite car ");
+        String car = scanner.nextLine();
+
+        System.out.println( "you are "+ age + " Years old");
+        System.out.println("you like the color " + color);
+        System.out.println("you like the car " + car);
 
 
-        System.out.print(" What is your GPA:? ");
-        double Gpa = scanner.nextDouble();
-
-        System.out.println("Are you a Student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean()
-;
-        System.out.println("Hello you are " + name + " And you are " + age + " Years old");
-        System.out.println("Your GPA is : " + Gpa);
-
-        if(isStudent){
-            System.out.println("Student: "+ isStudent);
-        }else{
-            System.out.println(" No Acess! " + "You are not Enrolled");
-        }
         scanner.close();
         //it is good to close your scanner at the end of your application
     }
