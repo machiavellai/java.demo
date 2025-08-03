1b33f1fc4c31369f66ad4b6aa444f8aec6144dd7
machiavellai <oghenevictor54p@gmail.com>
2025-08-03 21:58:54 +0100

    Working with Random class

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,36 @@
-import  java.util.Scanner;
+import java.util.Random;
+//importing random class
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       //if styatement = perform a block of code if the condition is true
+       //working and genrating Random numbers
 
-        Scanner scanner = new Scanner(System.in);
+        //initializing the random object
+        Random random = new Random();
 
-        String name;
-        int age = 40;
-        boolean isStudent;
+        boolean isHeads ;
+        int number1 ;
+        int number2 ;
+        int number3 ;
 
-        System.out.print("Enter your name: ");
-        name = scanner.nextLine();
+        //setting a bounds for the random number for range
+        // 6 in exclusive and 1 is inclusive( origin && Bound)
+        isHeads = random.nextBoolean();
 
-        System.out.print(" Enter your AGe: ");
-        age = scanner.nextInt();
-
-
-        System.out.println("Are you a student (true/false): ");
-        isStudent = scanner.nextBoolean();
-
-        //Group1
-        // to check if there's a value for the name
-        //string method to check if tjere's a value for a string or if it's empty
-        if(name.isEmpty()){
-            System.out.println("you didn't enter you name");
+        if(isHeads){
+            System.out.println("Heads");
+        }else{
+            System.out.println("Tails");
         }
-        else{
-            System.out.println("hello " + name);
-        }
+        number1 = random.nextInt(1, 7);
+        number2 = random.nextInt(1,8);
+        number3 = random.nextInt(2, 8);
 
-        //group 2
-        if(age >= 70){
-            System.out.println("you are a senior 👴🏽");
-        } else if (age >= 18) {
-            System.out.println("you are an adult🧔🏽‍");
-        } else if (age < 0) {
-            System.out.println("you are not yet born");
-        }
-        else if (age == 0) {
-            System.out.println("you are a baby");
-        } else{
-            System.out.println("you are child sucker!");
-        }
+//        System.out.println(isHeads);
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
 
-        //Group 3
-        if( isStudent){
-            System.out.println("you are a student");
-        }
-        else{
-            System.out.println("you are not a student");
-        }
-
-        scanner.close();
     }
 }
