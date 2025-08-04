ac9837badee5f961ce460e90da2613e6ec37663c
machiavellai <oghenevictor54p@gmail.com>
2025-08-05 00:06:29 +0100

    nested loops(for loops)#2

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)  {
 
-        //nested loop = A loop inside another loop
-        //              often used with Matrices or DS&A
+       //Using nested loops to work with arrays
 
-        //if you have a loop inside another loop give different variable name
-        for( int i = 1; i <=9; i++){
-            for( int j = 1; j <=9; j++){
-                System.out.print(j + " ");
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbols;
+
+        System.out.print("ENter the number of Rows: " );
+        rows = scanner.nextInt();
+
+        System.out.print("Enter the number of Columns: " );
+        columns = scanner.nextInt();
+
+        System.out.print("Enter the symbok to use: ");
+        symbols = scanner.next().charAt(0);
+
+        //setting up the matrices
+        //starting with columns
+        //outer loop for
+        for (int j =0; j < rows; j++){
+            for (int i = 0; i < columns; i++){
+                System.out.print(symbols);
             }
             System.out.println();
         }
+
+        scanner.close();
+//        //nested loop = A loop inside another loop
+//        //              often used with Matrices or DS&A
+//
+//        //if you have a loop inside another loop give different variable name
+//        for( int i = 1; i <=9; i++){
+//            for( int j = 1; j <=9; j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
 //
 
 // break = break out of a loop (STOP)
