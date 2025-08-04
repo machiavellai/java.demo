80c9a5516b44031ccc42086bc4f1ca1fa8c9accf
machiavellai <oghenevictor54p@gmail.com>
2025-08-04 01:39:52 +0100

    nested if

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Compound Interest calculator
-        Scanner scanner = new Scanner(System.in);
 
-        double principle;//principle amount
-        double rate;
-        int timesCompounded;
-        int years;
-        double amount;
+        //mastering the nested if section
+        boolean isStudent = false;
+        boolean isSenior = true;
+        double price = 9.99;
 
-        System.out.print("enter the principal amount : " );
-        principle = scanner.nextDouble();
-
-        System.out.print(" Enter the interest rate (in %): ");
-        rate = scanner.nextDouble() / 100 ; //for the division of percentile
-
-        System.out.print("Enter the # of times compounded per year: ");
-        timesCompounded = scanner.nextInt();
-
-        System.out.print(" ENter the Amount of years: ");
-        years = scanner.nextInt();
-
-        amount = principle * Math.pow(1 + rate/timesCompounded, timesCompounded * years);
-
-        System.out.printf("The Amount after  %d years is $%,.0f ", years, amount);
-
-        scanner.close();
+        if(isStudent){
+            if(isSenior){
+                System.out.println("You get a senior discount of %20\n");
+                System.out.println("You also get a student account of %10");
+                price *= 0.7;
+            }else{
+                System.out.println("you get a student discount of 10%");
+                price  *= 0.9;
+            }
+        }else{
+            if(isSenior){
+                System.out.println("you get a senior discount of 20%");
+                price *= 0.8;
+            }
+            else{
+                price *= 1;
+            }
+        }
+        System.out.printf("The Price of a ticket is: $%.2f" , price);
     }
 }
