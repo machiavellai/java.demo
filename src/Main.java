a0dda2b628baf25966bbbe53b679cee30b396b28
machiavellai <oghenevictor54p@gmail.com>
2025-08-04 18:49:44 +0100

    for loop!

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,38 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        //Number Guessding Game
+    public static void main(String[] args) throws  InterruptedException {
 
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        // guess
-        int guess;
-        int attempts =0;
-        int min = 1;
-        int max = 100;
-        int randomNumber = random.nextInt(min, max + 1);
+        System.out.println("How many seconds to countdown from?: ");
+        int start = scanner.nextInt();
 
-        System.out.println("<<<<<<<<<< Number Guessing Game >>>>>>>>>>>>>");
-        System.out.printf("Guess a number between %d - %d: \n", min, max);
+        for(int i = start ; i > 0; i--){
+            System.out.println(i);
+            //implementing  threads is a java class used to cause a delay in output
+            Thread.sleep(1000);
+        }
+        System.out.println("Happy new Year!!");
 
-        do {
-        //do the condition here...keep playing
-            System.out.print("Enter A Guess: ");
-            guess = scanner.nextInt();
-            attempts++;
-//ll
-            //check to see if the guess is less than the random number and various conditions
-            if (guess < randomNumber){
-                System.out.println("Too low Try AGain");
-            }
-            else if (guess > randomNumber){
-                System.out.println("Too High Try AGain");
-            }
-            else {
-                System.out.println("You Won 🥇");
-                System.out.println("Correct the number was : " + randomNumber);
-                System.out.println("Number of Attempts " + attempts);
-            }
-        }while (guess != randomNumber); //while the guess doesn't equal to our random guess
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter How many times you want to loop: ");
+//        int max = scanner.nextInt();
+//
+//            for (int i = 0; i < max; i ++){
+//                System.out.println(i);
+//            }
 
-        scanner.close();
+
+//        // for loop = execute some code a  CERTAIN amount of times
+//        //while loop executes an infinte number of times until it's no longer valid
+//
+//        //we declare a counter first in the loop or a loop control var
+//        //Condtion Variable is second
+//        for (int i =10; i >0; i-=2 ){
+//            System.out.println(i);
+
+//        scanner.close();
+        }
     }
-}
