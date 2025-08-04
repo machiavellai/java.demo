48355cbe6e0c6ed12c79b6f494c99b43276628ee
machiavellai <oghenevictor54p@gmail.com>
2025-08-04 17:29:15 +0100

    while condition

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner((System.in));
 
-        String response = "";
+      int number = 0;
 
-        while(!response.equals("Q")){
-            System.out.println("You are playing a Game");
-            System.out.print(" Press Q to quit: ");
-            response = scanner.next().toUpperCase();
-        }
+      do{
+          System.out.print("enter a umber betwee 1-10: ");
+          number = scanner.nextInt();
 
-        System.out.println("You have to quit the Game");
+      }while  (number < 1 || number > 11);
+        System.out.println(" You picked " + number);
+//        //using while loop to make sure no one uses a negative value
+//        while(age < 0){
+//            System.out.println("Your Age can't be negative");
+//            System.out.print("Enter your age: ");
+//            age = scanner.nextInt();
+//        }
+//
+//        //now working with do-while loops
+//        do {
+//            System.out.println("Your Age can't be negative");
+//            System.out.print("Enter your age: ");
+//            age = scanner.nextInt();
+//        }while(age < 0);
+
+        //when the age meet the requirements we then accept the input
+//        System.out.println("Your are " + age + " Years old");
+
+        scanner.close();
     }
 }
