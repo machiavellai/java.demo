cbee269fae9b4cc2d8e695fa068a88ca9b9a55c4
machiavellai <oghenevictor54p@gmail.com>
2025-08-04 17:09:56 +0100

    while condition

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-      Scanner scanner = new Scanner(System.in);
+        String response = "";
 
-      //username must be btw 4-12 characters
-        //username must not contain spaces or underscores
-
-
-        String username;
-
-        System.out.print("Enter your username: ");
-        username = scanner.nextLine();
-
-        if(username.length() < 4 || username.length() >12){
-            System.out.println("Username must be between 4 - 12 characters");
-        } else if(username.contains(" ") || username.contains("_")) {
-            System.out.println("Username must not contain Spaces or UnderScores");
+        while(!response.equals("Q")){
+            System.out.println("You are playing a Game");
+            System.out.print(" Press Q to quit: ");
+            response = scanner.next().toUpperCase();
         }
 
-        else {
-            System.out.println("Welcome : " + username);
-        }
+        System.out.println("You have to quit the Game");
     }
 }
