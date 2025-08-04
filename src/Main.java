2ed3950251e41d5dc9b6966eb0eeaa2891028d51
machiavellai <oghenevictor54p@gmail.com>
2025-08-04 01:23:01 +0100

    compound interest

--- src/Main.java
+++ src/Main.java
@@ -2,58 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //printf() = is a method used to format output
+        //Compound Interest calculator
+        Scanner scanner = new Scanner(System.in);
 
-        // %[flags][width][.precision][specifier-character]
+        double principle;//principle amount
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
 
-        String name = "SpongeBob";
-        char firstLetter = 'S';
-        int age = 30;
-        boolean isEmployee = true;
-        double Height = 60.5;
+        System.out.print("enter the principal amount : " );
+        principle = scanner.nextDouble();
 
-        // now working[.precision]
-        double price1 = 90.99;
-        double price2 = 10.14;
-        double price3 = -220.3;
+        System.out.print(" Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100 ; //for the division of percentile
 
-        // now working[.width]
-        int id1 = 99;
-        int id2 = 14;
-        int id3 = 203;
-        int id4 = 2103;
+        System.out.print("Enter the # of times compounded per year: ");
+        timesCompounded = scanner.nextInt();
 
+        System.out.print(" ENter the Amount of years: ");
+        years = scanner.nextInt();
 
-        System.out.printf("Hello %s\n" , name );
-        System.out.printf("Your Name Starts with a %c\n" ,firstLetter );
-        System.out.printf("Your are %d years old\n" , age );
-        System.out.printf("You are %1f inches tall\n", Height);
-        System.out.printf("Employed:  %b\n", isEmployee);
+        amount = principle * Math.pow(1 + rate/timesCompounded, timesCompounded * years);
 
-        //combining different value of different datatype in the formatted string
-        //setting the precision to the amount of numbers we want shown
-        //output a plus (flags) for positive numbers
+        System.out.printf("The Amount after  %d years is $%,.0f ", years, amount);
 
-        // + = couput a plus
-        // , = comma grouping sperators
-        // ( = negative numbers enclosed in ()
-        // space = display a minus if negative, space if positive
-
-        System.out.printf("%s is %d years old \n", name, age);
-        System.out.printf("% .1f \n", price1);
-        System.out.printf("% .2f\n", price2);
-        System.out.printf("% .1f\n", price3);
-
-
-        //working with width
-        //0 = zero padding then add the amount of digits you want e.g.  printf("%0d\n", id1);
-        // number = right justified padding  e.g printf("%4d\n", id2);
-        // negative number = left justified padding e.g printf("%-4d\n", id1);
-
-        System.out.println("Working with the width width formatted strings");
-        System.out.printf("%04d\n", id1);
-        System.out.printf("%04d\n", id2);
-        System.out.printf("%03d\n", id3);
-        System.out.printf("%04d\n", id4);
+        scanner.close();
     }
 }
