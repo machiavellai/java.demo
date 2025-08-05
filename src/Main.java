daa82ddeea063786e6be16e9cd2d09de27ceb06c
machiavellai <oghenevictor54p@gmail.com>
2025-08-05 10:38:17 +0100

    banking programme

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,106 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
-    static  int x = 3; //Class
+   static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args)  {
-        //variable scope
+        // JAVA BANKING PROGRAM FOR BEGINNERS
+        Scanner scanner = new Scanner(System.in);
+        double balance = 0; //in case of.99 in amount
+        boolean isRunning = true;
+        int choice;
 
-        int x = 1;//local Var
-        System.out.println(x);
+        //the interactive part of the code
+        //i want the programm to run as long as is running= true
+        // hence a while loop
 
-        doSomething();
+        while (isRunning){
+            System.out.println("******************");
+            System.out.println("BANKING PROGRAMME");
+            System.out.println("******************");
+            System.out.println("1. Show balance: ");
+            System.out.println("2. Deposit: ");
+            System.out.println("3. Withdraw: ");
+            System.out.println("4. Exit: ");
+            System.out.println("******************");
+
+            System.out.println("Enter your choice (1 -4): ");
+            choice = scanner.nextInt();
+
+            switch (choice){
+                //in case one we will pass the showBalance method ...
+                case  1 -> {
+                   showBalance(balance);
+                }
+                case 2 ->{
+                    balance += deposit();
+                }
+                case 3 ->{
+                   balance -= withdraw(balance);
+                }
+                case 4 ->{
+                    isRunning = false;
+                }
+                default -> System.out.println("INVALID CHOICE");
+            }
+        }
+        System.out.println("******************");
+        System.out.println("THANK YOU HAVE A NICE DAY");
+
+
+        //withdraw()
+
+        // DECLARE VAR
+
+        //DISPLAY MENU
+
+        //USERS PICK A CHOICE
+
+        // GET AND PROCESS USERS CHOICE
+
+        //SHOWBalance()
+
+        //deposit()
+
+
+        //EXIT MESSAGE
     }
-    static void doSomething(){
-        int x = 2;
-        //another local variable
-
-        System.out.println(x);
+    static void showBalance(double balance){
+        System.out.println("******************");
+        System.out.printf("$%.2f balance \n ", balance );
     }
 
+    static double deposit(){
+        double amount;
+        System.out.print("Enter an Amont to b deposited: ... $");
+        //we need to get deposit but we can't access the scaneer method
+        // from the other methods because we can't see  the other variables
 
+        amount = scanner.nextDouble();
+
+        if (amount < 0){
+            System.out.println("Amount can't be negative!");
+            return  0;
+
+        }else{
+            return  amount;
+        }
+    }
+
+    static  double withdraw(double balance){
+        double amount;
+        System.out.print("ENter an Amount to be withdraw: ");
+        amount = scanner.nextDouble();
+
+        if(amount >balance)    {
+            System.out.println("INSUFFICIENT FUNDS");
+            return  0;
+        } else if (amount < 0) {
+            System.out.println("Amount can't be negative");
+            return  0;
+        }
+        else{
+            return  amount;
+        }
+    }
 }
