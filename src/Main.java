d99e55537f75efcf3bca16ef5acd5279448a1d5d
machiavellai <oghenevictor54p@gmail.com>
2025-08-05 09:19:39 +0100

    overloaded methods#1

--- src/Main.java
+++ src/Main.java
@@ -3,53 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)  {
+        //overloaded methods = methods that share
+        // the same name but different parameters
+        //      signature = name + parameters
 
-        //method is a block of reusable block of code that is executed when called()
+        System.out.println(add(3, 6));
+        System.out.println(add(3, 6, 4));
 
-       double result =  square(3);
-        System.out.println(result);
 
-        String FUllName = getFullName("Victor", "Oghene");
-        System.out.println(FUllName);
-
-        int age = 10;
-        if (ageCheck(age)){
-            System.out.println("You may Sign up!");
-        }else{
-            System.out.println("You must be 18+ to sign up");
-        }
     }
-    //declaring the method
-    //to do an action
-    // it needs a return type, name for method and parenthesis
-    // adding "static void" because we are in a static class
-
-    static void happyBirthday(String name, int age){
-        System.out.println("happy Birthday to you");
-        //there's an error because methods are not
-        // familiar with methods in another variable
-        //it's like 2 neighbours on the same street
-
-        System.out.printf("happy Birthday Dear %s!\n", name);
-        System.out.printf("you Are %d yars old\n", age );
-        System.out.println("happy Birthday to you\n");
+    static double add (double a, double b){
+        return  a + b;
     }
-    static  double square(double number){
-        return  number * number ;
+    //the same method name but different parameters
+    //no two methods can share two signatures
+    static double add (double a, double b, double c){
+        return  a + b + c;
     }
-    static double cube (double number ){
-        return number * number * number;
-    }
-    static String getFullName(String first, String last){
-        return first + " " + last;
-    }
-    static  boolean ageCheck(int age){
-        if (age >= 18){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-
 }
