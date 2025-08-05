6101eb922fee3c14f5f4f08420a41e414f6a11cf
machiavellai <oghenevictor54p@gmail.com>
2025-08-05 09:30:50 +0100

    overloaded methods#2

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,17 @@ public class Main {
         // the same name but different parameters
         //      signature = name + parameters
 
-        System.out.println(add(3, 6));
-        System.out.println(add(3, 6, 4));
+        String pizza = bakePizza("Flat Bread", "mozzarella", "Pepperoni");
+        System.out.println(pizza);
+    }
+    static String bakePizza ( String Bread){
+        return Bread + "  Pizza";
+    }
+    static String bakePizza ( String Bread, String cheese){
+        return cheese + " " + Bread + "Pizza";
+    }
+    static String bakePizza ( String Bread, String cheese, String Topping){
+        return Topping+ " "+ cheese + " " + Bread + " "+ "Pizza";
+    }
 
-
-    }
-    static double add (double a, double b){
-        return  a + b;
-    }
-    //the same method name but different parameters
-    //no two methods can share two signatures
-    static double add (double a, double b, double c){
-        return  a + b + c;
-    }
 }
