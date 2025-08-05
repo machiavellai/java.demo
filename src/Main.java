03ca1d4596b11e7ea5fdaae519bbf0fc08cdbeeb
machiavellai <oghenevictor54p@gmail.com>
2025-08-05 09:53:21 +0100

    local and class variables

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)  {
-        //overloaded methods = methods that share
-        // the same name but different parameters
-        //      signature = name + parameters
 
-        String pizza = bakePizza("Flat Bread", "mozzarella", "Pepperoni");
-        System.out.println(pizza);
+    static  int x = 3; //Class
+
+    public static void main(String[] args)  {
+        //variable scope
+
+        int x = 1;//local Var
+        System.out.println(x);
+
+        doSomething();
     }
-    static String bakePizza ( String Bread){
-        return Bread + "  Pizza";
+    static void doSomething(){
+        int x = 2;
+        //another local variable
+
+        System.out.println(x);
     }
-    static String bakePizza ( String Bread, String cheese){
-        return cheese + " " + Bread + "Pizza";
-    }
-    static String bakePizza ( String Bread, String cheese, String Topping){
-        return Topping+ " "+ cheese + " " + Bread + " "+ "Pizza";
-    }
+
 
 }
