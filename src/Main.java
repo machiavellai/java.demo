dfda40505129c2763f2ea02424427cc91df7e463
machiavellai <oghenevictor54p@gmail.com>
2025-08-05 11:53:11 +0100

    Arrays beginner

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,36 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args)  {
-        //JAVA DICE ROLLER PROGRAMME
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
-        //DECLARE Variables
-        int numOfDice;
-        int total = 0;
+        // working with ARRAY
 
-        //GET # OF DICE FROM THE USERS
-        System.out.print("Enter the # of dice to roll: ");
-        numOfDice = scanner.nextInt();
+        String[] fruits = {"apple", "orange", "coconut", "tuyyu"};
 
-        //CHECK IF THE # OF DICE > 0
-        if (numOfDice >0){
-            //ROLL THE DICE
-            for(int i =0; i < numOfDice; i++){
-                int roll = random.nextInt(1, 7);
-                printDie(roll);
-                System.out.println(" You rolled : " + roll);
-                //GET THE TOTAL
-               total += roll;
-            }
-            System.out.println("Total na: " + total);
-        }else{
-            System.out.println(" # of dice must be greater than 0");
-        }
+//        fruits[1] = "pineapple";
+       // int numOfFruits = fruits.length;
+
+        //using for loop to list through the list of array
+//        for (int i = 0; i < fruits.length; i++) {
+//            //printing each elemts of the array with index
+//            System.out.print(fruits[i] + " ");
+//        }
 
 
-        scanner.close();
-    }
-    // DISPLAY THE ASCII OF DICE
-    static void printDie(int roll){
-        String dice1 = """
-                 -------
-                |       |
-                |   ◉   |
-                |       |
-                 -------
-                """;
-        String dice2 = """
-                 -------
-                |◉      |
-                |   ◉   |
-                |       |
-                 -------
-                """;
+        //sorting your arrays
+//        Arrays.sort(fruits);
 
-        String dice3 = """
-                 -------
-                |◉     ◉|
-                |       |
-                |      ◉|
-                 -------
-                """;
+        //fill method with Arrays
+        Arrays.fill(fruits, "pineapple");
 
-        String dice4 = """
-                 -------
-                |◉     ◉|
-                |       |
-                |◉     ◉|
-                 -------
-                """;
-        String dice5 = """
-                 -------
-                |◉     ◉|
-                |   ◉   |
-                |◉     ◉|
-                 -------
-                """;
-        String dice6 = """
-                 -------
-                |◉     ◉|
-                |◉     ◉|
-                |◉     ◉|
-                 -------
-                """;
-        switch (roll){
-            case 1 ->{
-                System.out.print(dice1);
-            }
-            case 2 ->{
-                System.out.print(dice2);
-            }
-            case 3 ->{
-                System.out.print(dice3);
-            }
-            case 4 ->{
-                System.out.print(dice4);
-            }
-            case 5 ->{
-                System.out.print(dice5);
-            }
-            case 6 ->{
-                System.out.print(dice6);
-            }
-            default -> System.out.println("nono");
+        //enhanced for loops/for each loop
+        //where you have to delcare the data types of your items in array
+        for(String fruit : fruits){
+            System.out.println(fruit);
+
         }
     }
 
