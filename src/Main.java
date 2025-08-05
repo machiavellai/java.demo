d678bb945843d6b4b7a136424645447cfc004515
machiavellai <oghenevictor54p@gmail.com>
2025-08-05 11:24:01 +0100

    dice roler

--- src/Main.java
+++ src/Main.java
@@ -2,102 +2,105 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-   static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args)  {
-        // JAVA BANKING PROGRAM FOR BEGINNERS
+        //JAVA DICE ROLLER PROGRAMME
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        // DECLARE VAR
-        double balance = 0; //in case of.99 in amount
-        boolean isRunning = true;
-        int choice;
+        //DECLARE Variables
+        int numOfDice;
+        int total = 0;
 
-        //the interactive part of the code
-        //i want the programm to run as long as is running= true
-        // hence a while loop
-        while (isRunning){
-            //DISPLAY MENU
-            System.out.println("******************");
-            System.out.println("BANKING PROGRAMME");
-            System.out.println("******************");
-            System.out.println("1. Show balance: ");
-            System.out.println("2. Deposit: ");
-            System.out.println("3. Withdraw: ");
-            System.out.println("4. Exit: ");
-            System.out.println("******************");
+        //GET # OF DICE FROM THE USERS
+        System.out.print("Enter the # of dice to roll: ");
+        numOfDice = scanner.nextInt();
 
-            //USERS PICK A CHOICE
-            System.out.println("Enter your choice (1 -4): ");
-            // GET AND PROCESS USERS CHOICE
-            choice = scanner.nextInt();
-
-            switch (choice){
-                //in case one we will pass the showBalance method ...
-                case  1 -> {
-                    //action
-                    //SHOWBalance()
-                   showBalance(balance);
-                }
-                case 2 ->{
-                    //action
-                    //deposit()
-                    balance += deposit();
-                }
-                case 3 ->{
-                    //action
-                    //withdraw()
-                   balance -= withdraw(balance);
-                }
-                case 4 ->{
-                    //action
-                    isRunning = false;
-                }
-                default -> System.out.println("INVALID CHOICE");
+        //CHECK IF THE # OF DICE > 0
+        if (numOfDice >0){
+            //ROLL THE DICE
+            for(int i =0; i < numOfDice; i++){
+                int roll = random.nextInt(1, 7);
+                printDie(roll);
+                System.out.println(" You rolled : " + roll);
+                //GET THE TOTAL
+               total += roll;
             }
-        }
-        //EXIT MESSAGE
-        System.out.println("******************");
-        System.out.println("THANK YOU HAVE A NICE DAY");
-
-
-    }
-    static void showBalance(double balance){
-        System.out.println("******************");
-        System.out.printf("$%.2f balance \n ", balance );
-    }
-
-    static double deposit(){
-        double amount;
-        System.out.print("Enter an Amont to b deposited: ... $");
-        //we need to get deposit but we can't access the scaneer method
-        // from the other methods because we can't see  the other variables
-
-        amount = scanner.nextDouble();
-
-        if (amount < 0){
-            System.out.println("Amount can't be negative!");
-            return  0;
-
+            System.out.println("Total na: " + total);
         }else{
-            return  amount;
+            System.out.println(" # of dice must be greater than 0");
+        }
+
+
+        scanner.close();
+    }
+    // DISPLAY THE ASCII OF DICE
+    static void printDie(int roll){
+        String dice1 = """
+                 -------
+                |       |
+                |   ◉   |
+                |       |
+                 -------
+                """;
+        String dice2 = """
+                 -------
+                |◉      |
+                |   ◉   |
+                |       |
+                 -------
+                """;
+
+        String dice3 = """
+                 -------
+                |◉     ◉|
+                |       |
+                |      ◉|
+                 -------
+                """;
+
+        String dice4 = """
+                 -------
+                |◉     ◉|
+                |       |
+                |◉     ◉|
+                 -------
+                """;
+        String dice5 = """
+                 -------
+                |◉     ◉|
+                |   ◉   |
+                |◉     ◉|
+                 -------
+                """;
+        String dice6 = """
+                 -------
+                |◉     ◉|
+                |◉     ◉|
+                |◉     ◉|
+                 -------
+                """;
+        switch (roll){
+            case 1 ->{
+                System.out.print(dice1);
+            }
+            case 2 ->{
+                System.out.print(dice2);
+            }
+            case 3 ->{
+                System.out.print(dice3);
+            }
+            case 4 ->{
+                System.out.print(dice4);
+            }
+            case 5 ->{
+                System.out.print(dice5);
+            }
+            case 6 ->{
+                System.out.print(dice6);
+            }
+            default -> System.out.println("nono");
         }
     }
 
-    static  double withdraw(double balance){
-        double amount;
-        System.out.print("ENter an Amount to be withdraw: ");
-        amount = scanner.nextDouble();
-
-        if(amount >balance)    {
-            System.out.println("INSUFFICIENT FUNDS");
-            return  0;
-        } else if (amount < 0) {
-            System.out.println("Amount can't be negative");
-            return  0;
-        }
-        else{
-            return  amount;
-        }
-    }
 }
