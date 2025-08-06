59db01712d81263e768e96960dbd0845dc651f12
machiavellai <oghenevictor54p@gmail.com>
2025-08-06 13:13:54 +0100

    2D Array #2

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,41 @@ public class Main {
      // 2D ARRAY = A Array is where each element is an array
         //useful for storing a matrix of data
 
-        String [] fruits = {"orange", "apple", "watermelon"};
-        String [] protein = {"meat", "chicken", "fish"};
-        String[] vegetables = {"Potatoes", "onion"," carrot"};
+    char [][] telephone = {{'1', '2', '3'},
+                           {'4', '5', '6'},
+                           {'7', '8', '9'},
+                          {'*', '0', '#'}};
 
-        String [] [] groceries = {fruits, vegetables, protein};
+    for (char[] row : telephone){
+        for (char number : row){
+        System.out.print(number + " ");
+    }
+    System.out.println();
+}
 
-        //replacing items in the multidimensional array
-        groceries[0][0] = "Pineapple";
-        groceries[2][0] = "Avocado";
-        groceries[2][2] = "beetruit";
 
-        //we have to use extended for loop because a normal for loop
-        // will return a memory location
-        for (String [] foods : groceries){
-           for(String food : foods){
-               System.out.print(food + " ");
-           }
-            System.out.println();
-        }
+
+
+
+//
+//        String [] fruits = {"orange", "apple", "watermelon"};
+//        String [] protein = {"meat", "chicken", "fish"};
+//        String[] vegetables = {"Potatoes", "onion"," carrot"};
+//
+//        String [] [] groceries = {fruits, vegetables, protein};
+//
+//        //replacing items in the multidimensional array
+//        groceries[0][0] = "Pineapple";
+//        groceries[2][0] = "Avocado";
+//        groceries[2][2] = "beetruit";
+//
+//        //we have to use extended for loop because a normal for loop
+//        // will return a memory location
+//        for (String [] foods : groceries){
+//           for(String food : foods){
+//               System.out.print(food + " ");
+//           }
+//            System.out.println();
+//        }
     }
 }
