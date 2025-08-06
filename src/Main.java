bc238455339d17d1f7337dcab3f89bdeb4a93a9c
machiavellai <oghenevictor54p@gmail.com>
2025-08-07 00:06:47 +0100

    overloading constructors

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args)  {
-                //Constructors = A special method to initialize objects
-                // You can pass arguments to a constructor
-                // and set up initial values
+        User user1 = new User("Spongebob");
+        User user2 = new User("Pat", "oghene.com");
+        User user3 = new User("slls", "oggh@gmail.com", 33);
+        User user4 = new User();
 
-        //calling the student class object
-        Student student1 = new Student("SPongebob", 30, 3.3);
-        Student student2 = new Student("Victor", 20, 2.9);
-        Student student3 = new Student("Sandy", 27, 4.0);
-
-        student1.study();
-        student2.study();
-        student3.study();
-        //we are trying to change the name make it a unique name
-        //So we need a constructor
+        System.out.println(user4.username);
+        System.out.println(user4.age);
+        System.out.println(user4.email);
 
 
+//        System.out.println(user1.username);
+//        System.out.println(user1.age);
+//        System.out.println(user1.email + "\n");
+//
+//        System.out.println(user2.username);
+//        System.out.println(user2.age);
+//        System.out.println(user2.email + "\n");
+//
+//        System.out.println(user3.username);
+//        System.out.println(user3.age);
+//        System.out.println(user3.email + "\n");
     }
 
 
