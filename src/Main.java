84b48088f66f35f44e8e4079fab56f9a9e5eb956
machiavellai <oghenevictor54p@gmail.com>
2025-08-06 23:33:16 +0100

    OOP & COnstructors

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args)  {
-                //OBJECT ORIENTED PROGRAMMING
-                //OBJ= an entity that holds data (attributes)and can perform actions(Methods)
-                // It is a reference data type
-                // class is a blueprint to create an object
+                //Constructors = A special method to initialize objects
+                // You can pass arguments to a constructor
+                // and set up initial values
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        //calling the student class object
+        Student student1 = new Student("SPongebob", 30, 3.3);
+        Student student2 = new Student("Victor", 20, 2.9);
+        Student student3 = new Student("Sandy", 27, 4.0);
 
-        //calling the car object defined
-        Car car = new Car();
-
-        //this will print a memory address because
-        // it's a reference data type
-
-        //modify the attributes of objects
-        car.isRunning = true;
-
-//        System.out.println(car.model);//dot. is a dot.operator
-//        System.out.println(car.make);   // allows you to access things in an object
-//        System.out.println(car.price);
-//        System.out.println(car.isRunning);
-//        System.out.println(car.year);
-
-        //you input a check for the changes before and after the actions
-        System.out.println("the engine is running: "+car.isRunning);
-        car.start();
-        System.out.println("the engine is running: "+car.isRunning);
-
-        car.stop();
-        System.out.println("the engine is running: "+car.isRunning);
-
-        //creating a new car object
-        Car car1 = new Car();
-        Car car2 = new Car();
-
-        System.out.println(car1.make + " " + car1.model);
-        System.out.println(car2.make + " " + car2.model);
+        student1.study();
+        student2.study();
+        student3.study();
+        //we are trying to change the name make it a unique name
+        //So we need a constructor
 
 
     }
