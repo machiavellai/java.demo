ea835c712a9f48b33096ea4666811f57b36affaf
machiavellai <oghenevictor54p@gmail.com>
2025-08-07 00:30:06 +0100

    Array of Objects

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args)  {
-        User user1 = new User("Spongebob");
-        User user2 = new User("Pat", "oghene.com");
-        User user3 = new User("slls", "oggh@gmail.com", 33);
-        User user4 = new User();
+            //Creating an Array of Objects
+        Car car1 = new Car("Mustang", "Red");
+        Car car2 = new Car("Corvette", "Blue");
+        Car car3 = new Car("Charger", "Yellow");
 
-        System.out.println(user4.username);
-        System.out.println(user4.age);
-        System.out.println(user4.email);
+        //anonymous object
+//        Car[] cars = {new Car("Mustang", "Red"),
+//                new Car("Corvette", "Blue"),
+//                new Car("Charger", "yello")};
 
+        //initializing the array object
+//        Car[] cars = new Car[3];
+         Car[] cars = {car1, car2, car3};
+         for( Car car : cars){
+             car.drive();
+         }
+         //using a for loop
+//        for (int i=0; i <cars.length; i++){
+//            cars[i].drive();
+//        }
 
-//        System.out.println(user1.username);
-//        System.out.println(user1.age);
-//        System.out.println(user1.email + "\n");
-//
-//        System.out.println(user2.username);
-//        System.out.println(user2.age);
-//        System.out.println(user2.email + "\n");
-//
-//        System.out.println(user3.username);
-//        System.out.println(user3.age);
-//        System.out.println(user3.email + "\n");
     }
 
 
