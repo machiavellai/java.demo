5d4f95d85ac1b4a4d5ea89332bfc0b0ec7f1b5ea
machiavellai <oghenevictor54p@gmail.com>
2025-08-06 12:33:01 +0100

    Arrays Operations #4

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,29 @@ public class Main {
 
     public static void main(String[] args)  {
 
+        Scanner scanner = new Scanner(System.in);
         //searching through an array
 
         int[] numbers = {1,9,8,5,6,3,2};
         //we need a target to search for
         String[] fruits = {"Orange", "apple", "Banana"};
-
-        String target2 = "Orange";
-        int target = 8;
-
-        //initialized a boolean operator for th found or not found case
         boolean isFound = false;
+        int target;
+        String target2;
+
+        System.out.print(" Enter a number to search for: ");
+        target = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print(" Enter a fruit to search for: ");
+        target2 = scanner.nextLine();
+        //initialized a boolean operator for the found or not found case
+
 
         //performing a linear search
         for ( int i = 0 ; i < numbers.length; i++){
             if (target == numbers[i]){
-                System.out.println("Element found at index : " + i);
+                System.out.println("Number found at index : " + i);
                 //we implement break here because
                 // we have found the element so it should stop searching
                 isFound = true;
@@ -32,7 +39,7 @@ public class Main {
         //working with search of array with integers
         for ( int i = 0 ; i < numbers.length; i++){
             if (fruits[i].equals(target2)){
-                System.out.println("Element found at index : " + i);
+                System.out.println( target2 + " Fruit found at index : " + i);
                 isFound = true;
                 break;
             }
@@ -41,6 +48,7 @@ public class Main {
         if (!isFound){
             System.out.println("Element not Found in the Array");
         }
+        scanner.close();
     }
 
 }
